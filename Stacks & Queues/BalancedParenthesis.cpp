478f9bc08d6d5e92fcc6a9def239c4bcda019308
Saumy26 <55891927+Saumy26@users.noreply.github.com>
2021-02-20 15:50:41 +0530

    Balanced Parenthesis Checker

---
+++ Stacks & Queues/BalancedParenthesis.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool ispar(string expr)
+{
+    stack<char> s; 
+    char x; 
+    for(int i=0; i<expr.length(); i++) { 
+        if (expr[i]=='('||expr[i]=='['||expr[i]=='{') { 
+            s.push(expr[i]); 
+            continue; 
+        } 
+        if (s.empty()) { 
+           return false; 
+        }
+        switch (expr[i]) { 
+        case ')': 
+            x = s.top(); 
+            s.pop(); 
+            if (x=='{' || x=='[') 
+                return false; 
+            break; 
+  
+        case '}': 
+            x = s.top(); 
+            s.pop(); 
+            if (x=='(' || x=='[') 
+                return false; 
+            break; 
+  
+        case ']': 
+            x = s.top(); 
+            s.pop(); 
+            if (x =='(' || x == '{') 
+                return false; 
+            break; 
+        } 
+    } 
+    return (s.empty()); 
+}
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       if(ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}  
