8365a24e901d59a627f40a0d830834ed8cd5fb6d
Saumy26 <55891927+Saumy26@users.noreply.github.com>
2021-02-21 09:33:58 +0530

    Preorder Traversal

---
+++ Trees/PreorderTraversal.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+vector<int> preorder(struct Node* root);
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+Node* buildTree(string str)
+{
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+    vector<string> ip;
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    Node* root = newNode(stoi(ip[0]));
+    queue<Node*> queue;
+    queue.push(root);
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+        Node* currNode = queue.front();
+        queue.pop();
+        string currVal = ip[i];
+        if(currVal != "N") {
+            currNode->left = newNode(stoi(currVal));
+            queue.push(currNode->left);
+        }
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+        if(currVal != "N") {
+            currNode->right = newNode(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() 
+{
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        vector<int> res = preorder(root);
+        for (int i : res)
+            cout << i << " ";
+        cout<<endl;
+    }
+    return 0;
+}
+
+void preOrder(struct Node* root)
+{
+  if (root==NULL) 
+    return;
+  cout << root->data << " "; 
+  preorder(root->left);  
+  preorder(root->right); 
+}
+
+vector <int> preorder(Node* root)
+{
+    vector <int> res;
+    preOrder(root);
+    return res;
+  
+}
